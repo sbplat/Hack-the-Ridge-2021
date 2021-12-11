f45619e7403c56e9fc79828655f44ee16027b38e
really-noob <yoloeric213@gmail.com>
2021-12-11 17:59:49 -0500

    get req

--- website/js/script.js
+++ website/js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
     console.log("loaded");
 
-    let currentCourseNum = 0;
+    const getURL = "";
 
     let info = {};
     let university = false;
@@ -32,28 +32,26 @@ $(document).ready(function() {
     });
 
     function appendCourseInfoField() {
-        currentCourseNum += 1;
-
         $("#course-form").append(`
             <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
                 <div class="relative flex-grow w-full">
                     <label for="course-code" class="leading-7 text-sm text-gray-600">Course Code</label>
                     <input
                         type="text"
-                        id="course-code${currentCourseNum}"
                         name="course-code"
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        placeholder="MCV4U1"
                     />
                 </div>
                 <div class="relative flex-grow w-full">
                     <label for="mark" class="leading-7 text-sm text-gray-600">Final Mark</label>
                     <input
                         type="number"
-                        id="mark${currentCourseNum}"
                         name="mark"
                         min="0"
                         max="100"
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        placeholder="${Math.floor(Math.random() * 101)}"
                     />
                 </div>
             </div>
@@ -103,13 +101,13 @@ $(document).ready(function() {
     }
 
     function getPrograms() {
-        return;
-        $.get(url, function(response, status) {
-            console.log(`${response} ${status}`);
+        $.get(getURL, info).done(function(data) {
+            console.log(data);
         });
+
+        updatePrograms();
     }
 
     function updatePrograms() {
-
     }
 });
