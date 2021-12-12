b0d7917944e8c96e21c45e84501929ecbfe84ed7
LiuGar <78517873+LiuGar@users.noreply.github.com>
2021-12-11 19:31:24 -0500

    Update input.py

--- api/input.py
+++ api/input.py
@@ -1,4 +1,3 @@
-
 import flask
 from flask import request, jsonify
 import json
@@ -33,22 +32,20 @@ def api_all():
 def api_mark():
     if 'mark' in request.args:
         mark = int(request.args['mark'])
-    else:
-        return "Error: Specifiy an ID."
     results = []
-
+    
     for uni in Udata['Universities']:
-        i = 0
-        for course in uni:
-            if course[2+i]['grade_needed'] <= mark:
-                    results.append(uni)
-            i += 1
+        
+         if int(uni["Algoma University"]["Accounting (BA 3 year)"]["grade_needed"]) <= mark:
+            print(mark)
+            results.append(uni)
     return jsonify(results)
 
 @app.route('/api/v1/resources/University', methods=['GET'])
 def api_id():
     # Check if an ID was provided as part of the URL.
     if 'id' in request.args:
+        
         id = int(request.args['id'])
     else:
         return "Error: Specify an ID."
@@ -58,11 +55,14 @@ def api_id():
 
     # Loop through the data and match results that fit the requested ID.
     # IDs are unique, but other fields might return many results
-    for uni in Udata['Universities']:
-        if uni['id'] == id:
-            results.append(uni)
+    for uni1 in Udata['Universities']:
+        for uni in uni1:
+            if uni['id'] == id:
+        
+                results.append(uni) 
 
     # Use the jsonify function from Flask to convert our list to JSON
     return jsonify(results)
 
 app.run()
+
